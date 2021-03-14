32f314e42ba16f58eb8014766a5ecc92f070f3bf
John Georgiadis <j.georgiadiss@gmail.com>
2021-03-14 11:37:06 +0100

    initial code

---
+++ scrollToTop.js
@@ -0,0 +1,27 @@
+const scrollFunction = () => {
+  const topButton = document.getElementById("topButton");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+};
+
+const goToTop = () => {
+  const topButton = document.getElementById("topButton");
+
+  topButton.addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+};
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => {
+  scrollFunction();
+};
+
+goToTop();
+
+
