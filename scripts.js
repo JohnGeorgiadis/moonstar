da7a2e7bf2996576bde8eb1e9c3b402beff87c0e
John Georgiadis <j.georgiadiss@gmail.com>
2021-03-14 14:30:56 +0100

    fix code style

--- scripts.js
+++ scripts.js
@@ -1,42 +1,40 @@
 const API = {
-  COMMENTS: "https://jsonplaceholder.typicode.com/comments",
-  POSTS: "https://jsonplaceholder.typicode.com/posts",
-  USERS: "https://jsonplaceholder.typicode.com/users"
+    COMMENTS: "https://jsonplaceholder.typicode.com/comments",
+    POSTS: "https://jsonplaceholder.typicode.com/posts",
+    USERS: "https://jsonplaceholder.typicode.com/users"
 };
 
 const addAccordionToComments = () => {
-  const commentCounter = document.getElementsByClassName("post__commentCount");
-  for (let i = 0; i < commentCounter.length; i++) {
-    commentCounter[i].addEventListener("click", function() {
-      const commentsContainer = this.nextElementSibling;
+    const commentCounters = document.getElementsByClassName("post__commentCount");
 
-      if (commentsContainer.style.display === "block") {
-        commentsContainer.style.display = "none";
-      } else {
-        commentsContainer.style.display = "block";
-      }
-    });
-  }
+    for (let commentCounter of commentCounters) {
+        commentCounter.addEventListener("click", function () {
+            const commentsContainer = this.nextElementSibling;
+            const isCommentsContainerVisible = commentsContainer.style.display === "block";
+
+            commentsContainer.style.display = isCommentsContainerVisible ? "none" : "block";
+        });
+    }
 };
 
 const populateComments = (commentData) => {
-  const commentContainer = document.getElementsByClassName("post__commentsContainer");
+    const commentContainer = document.getElementsByClassName("post__commentsContainer");
 
-  commentData.forEach((comment) => {
-    commentContainer[commentContainer.length - 1].innerHTML += `
+    commentData.forEach((comment) => {
+        commentContainer[commentContainer.length - 1].innerHTML += `
       <div class="post__comment">
         <div class="post__commenter">${comment.email}</div>
         <div class="post__commentName">${comment.name}</div>
         <div class="post__commentBody">${comment.body}</div>
       </div>
     `;
-  });
+    });
 };
 
 const populateCard = (cardData) => {
-  const container = document.getElementsByClassName("postsContainer");
+    const container = document.getElementsByClassName("postsContainer");
 
-  container[0].innerHTML += `
+    container[0].innerHTML += `
     <div class="post">
       <div class="post__username">${cardData.user.username}</div>
       <div class="post__title">${cardData.title}</div>
@@ -48,49 +46,50 @@ const populateCard = (cardData) => {
       </div>
     </div>
   `;
-  populateComments(cardData.comments);
+    populateComments(cardData.comments);
 };
 
 const setLoader = (isLoading) => {
-  const loader = document.getElementsByClassName("postsContainer__loader");
-  loader[0].style.display = isLoading ? "block" : "none";
+    const loader = document.getElementsByClassName("postsContainer__loader");
+    loader[0].style.display = isLoading ? "block" : "none";
 };
 
 const setError = (error) => {
-  const errorPlaceholder = document.getElementsByClassName("postsContainer__error");
-  errorPlaceholder[0].style.display = error ? "block" : "none";
+    const errorPlaceholder = document.getElementsByClassName("postsContainer__error");
+    errorPlaceholder[0].style.display = error ? "block" : "none";
 
-  if (error.length) {
-    errorPlaceholder[0].innerHTML = `<p>${error}</p>`;
-  }
+    if (error.length) {
+        errorPlaceholder[0].innerHTML = `<p>${error}</p>`;
+    }
 };
 
 const fetchApiData = async (dataUrl) => {
-  let results;
-  setError("");
-  setLoader(true);
+    let results;
+    setError("");
+    setLoader(true);
 
-  try {
-    const data = await fetch(dataUrl);
-    results = await data.json();
-  } catch (e) {
-    setError(e);
-  }
+    try {
+        const data = await fetch(dataUrl);
+        results = await data.json();
+    } catch (e) {
+        setError(e);
+    }
 
-  setLoader(false);
-  return results;
+    setLoader(false);
+    return results;
 };
 
 Promise.all([fetchApiData(API.POSTS), fetchApiData(API.COMMENTS), fetchApiData(API.USERS)]).then((values) => {
-  let postCardData = {};
-  const [posts, comments, users] = values;
+    const [posts, comments, users] = values;
 
-  posts.forEach((post) => {
-    postCardData = post;
-    postCardData.comments = comments.filter((comment) => comment.postId === post.id);
-    [postCardData.user] = users.filter((user) => user.id === post.userId);
-    populateCard(postCardData);
-  });
+    posts.forEach((post) => {
+        let postCardData;
 
-  addAccordionToComments();
+        postCardData = post;
+        postCardData.comments = comments.filter((comment) => comment.postId === post.id);
+        [postCardData.user] = users.filter((user) => user.id === post.userId);
+        populateCard(postCardData);
+    });
+
+    addAccordionToComments();
 });
--- scrollToTop.js
+++ scrollToTop.js
@@ -1,27 +1,27 @@
-const scrollFunction = () => {
-  const topButton = document.getElementById("topButton");
+const setScrollToTopButtonVisibility = () => {
+    const topButton = document.getElementById("topButton");
 
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topButton.style.display = "block";
-  } else {
-    topButton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
 };
 
-const goToTop = () => {
-  const topButton = document.getElementById("topButton");
+const addScrollToTopButtonListener = () => {
+    const topButton = document.getElementById("topButton");
 
-  topButton.addEventListener("click", function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
+    topButton.addEventListener("click", function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
 };
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = () => {
-  scrollFunction();
+    setScrollToTopButtonVisibility();
 };
 
-goToTop();
+addScrollToTopButtonListener();
 
 
